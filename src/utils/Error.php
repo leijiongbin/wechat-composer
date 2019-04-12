c4f721e60d70a751d1dd12eb80a65ae86d113a5a
Ryan <leijb@17uhui.com>
2019-04-12 11:57:42 +0800

    'init'

---
+++ src/utils/Error.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Error 错误代码类
+ *
+ */
+namespace wechat\utils;
+
+class Error {
+    
+    /**
+     * 获取某个错误的对象数组
+     *
+     * @return object(err, NULL)
+     *
+     * Examples:
+     * ```
+     * Error::code('ERR_GET');
+     * ```               
+     */
+    static public function code ($code)
+    {
+
+        // 本SDK自定义错误类型
+        $code_arr = array(
+            // 错误: get方式请求api网络错误
+            'ERR_GET' => array(13001, 'http get api error.'),
+            // 错误: post方式请求api网络错误     
+            'ERR_POST' => array(13002, 'http post api error.'),
+            // 错误: 消息类型未定义
+            'ERR_MEG_TYPE' => array(13003, 'message type is not defined.')
+        );
+        
+        return array((object)array(
+                'errcode' => $code_arr[$code][0],
+                'errmsg' => $code_arr[$code][1]
+            ), NULL);
+    }
+}
---
+++ src/utils/Xml.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Xml class
+ *
+ * Xml处理
+ *
+ */
+namespace wechat\utils;
+
+class Xml
+{
+    /**
+     * 生成xml字符串
+     * @param array $params 数据数组
+     * @return string
+     */
+    public static function toXml($params)
+    {
+        $xml = '<xml>';
+        foreach ($params as $key => $val) {
+            if (is_numeric($val)) {
+                $xml .= '<'.$key.'>'.$val.'</'.$key.'>';
+            } else {
+                $xml .= '<'.$key.'><![CDATA['.$val.']]></'.$key.'>';
+            }
+        }
+        $xml .= '</xml>';
+        return $xml;
+    }
+}
